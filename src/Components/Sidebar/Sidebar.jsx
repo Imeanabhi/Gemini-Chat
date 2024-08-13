8117c3d134a0184b0389202893f50de24d18c336
Imeanabhi <abhi.nagabothu@gmail.com>
2024-08-13 20:14:34 +0530

    hi

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,42 +1,51 @@
-import React from 'react';
+import React , {useState}  from 'react';
 import './Sidebar.css';
 import { assets } from '../../assets/assets';
+//hi
 const Sidebar = () => {
+
+
+    //for extending the sidebar
+const[extended,setExtended] = useState(false)
   return (
     <div className="sidebar">
         <div className="top">
-        <img className="Menu"src={assets.menu_icon} alt=""/>
+        {/*Here prev=>!prev is written because when the sidebar is opened and when we want to close
+        sidebar its opposite logic should be implemented  */}
+        <img onClick={()=>setExtended(prev=>!prev)} className="Menu"src={assets.menu_icon} alt=""/>
             <div className="new-chat">
                 <img src={assets.plus_icon} alt=""/>
-                    <p>
-                    New Chat
-                    </p> 
+                    {extended?<p>New Chat</p>:null}
             </div>
-    <div className="recent">
-        <div className="recent-title">
+
+    {extended?<div className="recent">
+        <p className="recent-title">Recent</p>  
+        <div className="recent-entry">
             <img src={assets.message_icon} alt=""/>
                 <p>What is react ....</p>
-        </div>
+    </div>
+    </div>
+    :null
+    }
     </div>
 <div className="bottom">
 <div className="bottom-item recent-entry">
-    <img src={assets.question_icon} alt=""/>
-    <p>Help</p>
+    <img className="questions" src={assets.question_icon} alt=""/>
+    {extended?<p>Help</p>:null}
 </div>
 
 <div className="bottom-item recent-entry">
     <img src={assets.history_icon} alt=""/>
-    <p>Activity</p>
+    {extended?<p>Activity</p>:null}
 </div>
 
 <div className="bottom-item recent-entry">
     <img src={assets.setting_icon} alt=""/>
-    <p>Settings</p>
+    {extended?<p>Settings</p>:null}
+</div>
+</div>
 </div>
 
-</div>
-        </div>
-    </div>
 )
 }
 
